76b99e9ca5b328a03f58ade40feaae1bec4c2a6e
castillorm <rod@keyesinfotech.com>
2026-02-10 14:35:59 -0500

    Updated the TransformTestHarness class.

--- qa/src/test/java/testutil/TransformTestHarness.java
+++ qa/src/test/java/testutil/TransformTestHarness.java
@@ -1,41 +1,47 @@
 package testutil;
 
+import com.rli.cragents.refresh.PipelineIdentifier;
+import com.rli.cragents.refresh.SyncPipelineTransformation;
+import com.rli.cragents.refresh.j;
+import com.rli.util.jndi.vdap.compound.CompoundObject;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 
-// Radiant doesn't expose public API for the entire setup process of a Transfrom
-// This file is the minima scaffolding required to setup a transfrom.
 public final class TransformTestHarness {
-  private static final String DECLARING_CLASS = "com.rli.cragents.refresh.SyncPipelineTransformation";
-  // The most notable private data that is required to setup is: 'pipelineMeta'
-  // by default this value is null, and there is no public API to set it.
-  private static final String FIELD_NAME = "pipelineMeta";
-  private static final Field PIPELINE_META_FIELD = resolvePipelineMetaField();
-
-  private TransformTestHarness() {
-  }
-
-  private static Field resolvePipelineMetaField() {
+  public static <T extends SyncPipelineTransformation> T newTestTransform(Class<T> transfoClass) {
     try {
-      Class<?> declaring = Class.forName(DECLARING_CLASS);
-      Field f = declaring.getDeclaredField(FIELD_NAME);
-      f.setAccessible(true);
-      return f;
-    } catch (Exception e) {
-      throw new ExceptionInInitializerError("Failed to resolve " + DECLARING_CLASS + "#" + FIELD_NAME + ": " + e);
-    }
-  }
-
-  public static <T> T newTestTransform(Class<T> transfoClass) {
-    try {
+      // 1) construct the transform
       Constructor<T> ctor = transfoClass.getDeclaredConstructor();
       ctor.setAccessible(true);
       T t = ctor.newInstance();
-      Object meta = PipelineMetaFactory.newTestPipelineMeta();
-      PIPELINE_META_FIELD.set(t, meta);
+      // 2) create minimal pipeline meta (j) via reflection
+      j meta = newMinimalPipelineMeta();
+      // 3) inject via the public setter
+      t.setPipelineMeta(meta);
       return t;
     } catch (Exception e) {
       throw new RuntimeException("Failed to create/inject test transform: " + transfoClass.getName(), e);
+    }
+  }
+
+  private static j newMinimalPipelineMeta() {
+    try {
+      Constructor<?> jConstructorFunction = j.class.getDeclaredConstructor(
+          String.class,
+          String.class,
+          CompoundObject.class,
+          PipelineIdentifier.class,
+          boolean.class,
+          boolean.class);
+      jConstructorFunction.setAccessible(true);
+      return (j) jConstructorFunction.newInstance(
+          "",
+          "",
+          null,
+          PipelineIdentifier.a("_pipeline_"),
+          false,
+          false);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create minimal pipeline meta", e);
     }
   }
 }
